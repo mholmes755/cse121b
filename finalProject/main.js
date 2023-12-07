f5f8fc3e55ccff522fce6216b801b7fe0841ebcc
mholmes755 <hol23026@byui.edu>
2023-12-06 21:35:55 -0700

    Updated code. Started on displaying Team Members

--- finalProject/main.js
+++ finalProject/main.js
@@ -1,14 +1,16 @@
 /* Declare and initialize global variabiles*/
 const teamsElement = document.getElementById("teams");
 let teamList = {};
+let teamMembersList = {};
 
-const displayTeams = (teams) =>
-{
+
+// Display Teams Function
+const displayTeams = (teams) =>{
     teams.forEach(team => {
         let article = document.createElement("article");
-        
-        let h3 = document.createElement("h3");
-        h3.textContent = team.teamName;
+
+        let name = document.createElement("h2");
+        name.textContent = team.teamName;
 
 
         let motive = document.createElement("p");
@@ -31,13 +33,84 @@ const displayTeams = (teams) =>
     });
 };
 
-// getTeams Function uising Fetch
+
+// Display Team Member Function
+const displayMembers = (members) =>{
+  members.forEach(member =>{
+    let article = document.createElement("article");
+
+    let memberName = document.createElement("h4");
+    let memberName.textContent = member.name;
+  });
+
+};
+
+// getTeams Function using Fetch
 const getTeams = async () => {
   const response = await fetch("https://mholmes755.github.io/CSE121B/finalProject/theMarvelRoster.json");
   teamList = await response.json();
   displayTeams(teamList);
 }
 
+// getTeamMembers Function using Fetch
+const getTeamMembers = async () => {
+  const response = await fetch("https://mholmes755.github.io/CSE121B/finalProject/theMarvelRoster.json");
+  teamMembersList = await response.json();
+  displayMembers(teamMembersList);
+}
 
+// Reset Function
+
+const reset = () => {
+  teamsElement.textContent = '';
+};
+
+// SortBy Function
+const sortBy = () =>{
+
+  reset();
+  
+  const filter = document.getElementById('#sortBy').Value;
+  switch (filter)
+  {
+    case "theAvengers":
+      let avengerTeam = teamList.filter(team => team.teamName.includes("Avengers"));
+      displayTeams(avengerTeam);
+      break;
+    
+    case "guardiansOfTheGalaxy":
+      let guardiansTeam = teamList.filter(team => team.teamName.includes("Guardians"));
+      displayTeams(guardiansTeam);
+      break;
+
+    case "sinisterSix":
+      let sinisterTeam = teamList.filter(team => team.teamName.includes("Sinister"));
+      displayTeams(sinisterTeam);
+      break;
+
+    case "mastersOfEvil":
+      let evilTeam = teamList.filter(team => team.teamName.includes("Evil"));
+      displayTeams(sinisterTeam);
+      break;
+
+    case "all":
+      displayTeams(teamList);
+      break;
+
+
+  }
+};
+
+
+
+
+// Selector Event Listener
+document.querySelector('#sortBy').addEventListener("change", ()=> {sortBy(teamList)});
+
+
+// Team Event Listener
+
+
+// Call main function
 getTeams();
 
